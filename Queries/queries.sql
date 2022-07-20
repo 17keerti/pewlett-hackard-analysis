e4433a9d2d1d66ec196fa70bda5a29393ec75c7f
Keerti Chaudhary <17keerti@gmail.com>
2022-07-20 01:09:27 -0700

    create new tables for emp retirement details with department name

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -87,3 +87,49 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     de.emp_no,
+     de.from_date,
+     de.to_date 
+FROM departments as d
+INNER JOIN dept_manager as de
+ON d.dept_no = de.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+    de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO emp_retirement
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
